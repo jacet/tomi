85cf84a44c8a87c4b207f7e5e5dc50d305a91e0e
Jason Tomic <jason.tomic@outlook.com>
2023-07-04 03:53:27 +1200

    updated

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,104 +1,34 @@
-import * as React from "react"
-import Orb from "../components/orb"
-import orb_img from "../images/orb.gif"
+import * as React from "react";
+import Orb from "../components/orb";
+import orb_img from "../images/orb.gif";
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-  background: "#000000"
-}
+  background: "#000000",
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  boxSizing: 'border-box',
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
 }
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  // {
-  //   text: "API Root",
-  //   url: "http://localhost:5000/api",
-  //   description:
-  //     "Default API endpoint",
-  //   color: "#E95800",
-  // }
-]
 
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        TOMI
-      </h1>
-      
-      <div>
-        <img src={orb_img} alt="orb"></img>
-      </div>     
+      <title>TOMI</title>
+      <img style={{maxWidth: '100%', height: 'auto'}} src={orb_img} alt="orb"></img>
     </main>
   )
 }
 
-export default IndexPage
-
+export default IndexPage;
 export const Head = () => <title>Home Page</title>
+
+
